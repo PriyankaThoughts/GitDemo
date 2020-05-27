69aed2e95d29ac145e63d2b5175612391bc863c4
priyanka <priyankarathore40@gmail.com>
2020-05-27 13:58:59 +0530

    first code

---
+++ src/test/java/PriyankaMaven/RESTApiTest.java
@@ -0,0 +1,18 @@
+package PriyankaMaven;
+
+import org.testng.annotations.Test;
+
+public class RESTApiTest {
+	
+	@Test
+	public void postjira()
+	{
+		System.out.println("postjira");
+	}
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+
+}
---
+++ src/test/java/PriyankaMaven/SeleniumTest.java
@@ -0,0 +1,18 @@
+package PriyankaMaven;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowerAutomation()
+	{
+		System.out.println("BrowerAutomation");
+	}
+	@Test
+	public void ElementsUI()
+	{
+		System.out.println("ElementsUI");
+	}
+
+}
